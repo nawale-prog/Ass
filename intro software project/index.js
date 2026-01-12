9421ed10e17fc47b3381437c73de20b2dda6c7d2
nawale-prog <nawale@student.refactory.academy>
2026-01-12 20:57:34 +0300

    Add files via upload

---
+++ intro software project/index.js
@@ -0,0 +1,9 @@
+console.log("Welcome to my Intro Software Project!");
+
+const projectName = "Intro Software Project";
+const author = "Nasra";
+const year = 2026;
+
+console.log("Project Name:", projectName);
+console.log("Author:", author);
+console.log("Year:", year);
